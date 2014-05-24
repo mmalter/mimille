ce9e617009c79d0547878c7ab2ec6454c4bc5439
Michaël Malter <dev@michaelmalter.fr>
2014-05-24 13:32:15 +0200

    Event loop implementation

--- mtorrent.py
+++ mtorrent.py
@@ -3,6 +3,8 @@ import configobj
 import validate
 import os
 import glob
+import socket
+import time
 
 #Use asyncio to communicate
 
@@ -30,6 +32,7 @@ def get_configuration(configuration_filename):
     complete_directory = string()
     session_directory = string()
     temporary_directory = string()
+    socket_directory = string()
     upload_download_ratio = int()"""
     configuration = configobj.ConfigObj(configuration_filename,
                                         configspec=_configspec.split('\n'))
@@ -41,15 +44,6 @@ def get_session(configuration):
     session = libtorrent.session()
     session.listen_on(configuration['port'])
     return session
-
-def initialize_torrents(configuration,session):
-    for torrent_path in glob.glob(
-        os.path.normpath(configuration.['torrent_directory']) '*.torrent'):
-        torrent = libtorrent.bdecode(open(torrent_path, 'rb').read())
-        info = libtorrent.torrent_info(torrent)
-        if info.info_hash() not in session.get_torrent_list():
-            yield session.add_torrent(info, configuration['download_directory'],
-                                     storage_mode='storage_mode_allocate')
 
 def save_session(session,configuration):
     filepath = os.path.normpath(configuration['temporary_directory']) 'session'
@@ -64,8 +58,37 @@ def load_session(configuration, session):
         session_state = libtorrent.bdecode(_file.read())
     session.load_state(session_state)
 
+def initialize_torrents(configuration,session):
+    for torrent_path in glob.glob(
+        os.path.normpath(configuration.['torrent_directory']) '*.torrent'):
+        torrent = libtorrent.bdecode(open(torrent_path, 'rb').read())
+        info = libtorrent.torrent_info(torrent)
+        if info.info_hash() not in session.get_torrent_list():
+            yield session.add_torrent(info, configuration['download_directory'],
+                                     storage_mode='storage_mode_allocate')
+
+def event_loop(configuration):
+    socket_path = os.path.normpath(configuration.['socket_directory'] '.socket'):
+    if os.path.exists( socket_path ):
+          os.remove( socket_path )
+    server = socket.socket( socket.AF_UNIX, socket.SOCK_STREAM )
+    server.bind(socket_path)
+    server.listen(5)
+    while True:
+        connection, address = server.accept()
+        while True: 
+            data = connection.recv( 512 )
+            if not data:
+                break
+            else:
+                print data
+                break
+
+
+
 if __name__ == "__main__":
     configuration = get_configuration(get_configuration_filename('mtorrent'))
     session = get_session(configuration)
     torrents = initialize_torrents(configuration,session)
+    event_loop(configuration)
 
